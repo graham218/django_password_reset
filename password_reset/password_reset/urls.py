95774d0da1779d61da78bc5a471b9ac2ad50b765
BILL GRAHAM <grahambill011@gmail.com>
2021-10-13 06:22:33 +0300

    Update urls.py

--- password_reset/password_reset/urls.py
+++ password_reset/password_reset/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup', views.signup, name="signup"),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('change_password', auth_views.PasswordChangeView.as_view(), name='change_password'),
+    path('password_change_done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done', auth_views.PasswordResetDoneView.as_view(),
+    name='password_reset_done'),
+    path('password_reset_confirm/<slug:uidb64>/<slug:token>/', auth_views.PasswordResetConfirmView.as_view(),
+    name='password_reset_confirm'),
+    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(),
+    name='password_reset_complete'),
+    path('',include('app.urls')),
 ]
